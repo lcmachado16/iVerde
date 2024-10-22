da1d47023c807d4f292b08b749a4491139a7ab58
lcmachado <lcmachado@inf.ufrgs.br>
2024-10-22 08:44:57 -0300

    first commit nostalgia

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Aqui você pode adicionar funcionalidades JavaScript, se necessário.
+    console.log("Página carregada. Sinta a nostalgia dos anos 2000!");
+});
